4845874e8af9aff69b44c1a4a3446bd05114f34b
maierhoferma81177@th-nuernberg.de <maierhoferma81177@th-nuernberg.de>
2021-10-28 16:18:18 +0200

    style sidebar icons

--- components/navbar.js
+++ components/navbar.js
@@ -6,19 +6,19 @@ export default function Navbar() {
     return (
         <>
             <div className={"w-20 h-screen bg-gray-800 flex-col justify-around items-center flex"}>
-                <div>
+                <div className="hover:bg-gray-700 text-lg p-3 rounded-lg cursor-pointer">
                     <FontAwesomeIcon icon={faHome} color="white"/>
                 </div>
-                <div>
+                <div className="hover:bg-gray-700 text-lg p-3 rounded-lg cursor-pointer">
                     <FontAwesomeIcon icon={faStream} color="white" />
                 </div>
-                <div>
+                <div className="hover:bg-gray-700 text-lg p-3 rounded-lg cursor-pointer">
                     <FontAwesomeIcon icon={faEuroSign} color="white" />
                 </div>
-                <div>
+                <div className="hover:bg-gray-700 text-lg p-3 rounded-lg cursor-pointer">
                     <FontAwesomeIcon icon={faUsers} color="white" />
                 </div>
-                <div>
+                <div className="hover:bg-gray-700 text-lg p-3 rounded-lg cursor-pointer">
                     <FontAwesomeIcon icon={faCog} color="white" />
                 </div>
             </div>
