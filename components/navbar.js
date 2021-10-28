0f90901770562f99b3361b96f040c2e9b840b368
maierhoferma81177@th-nuernberg.de <maierhoferma81177@th-nuernberg.de>
2021-10-28 15:41:11 +0200

    add icons to sidebar

--- components/navbar.js
+++ components/navbar.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faStream, faEuroSign, faUsers, faCog } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
     return (
         <>
-            <div className={"w-20 h-screen bg-gray-800"}>
-                Navbar
+            <div className={"w-20 h-screen bg-gray-800 flex-col justify-around items-center flex"}>
+                <div>
+                    <FontAwesomeIcon icon={faHome} color="white"/>
+                </div>
+                <div>
+                    <FontAwesomeIcon icon={faStream} color="white" />
+                </div>
+                <div>
+                    <FontAwesomeIcon icon={faEuroSign} color="white" />
+                </div>
+                <div>
+                    <FontAwesomeIcon icon={faUsers} color="white" />
+                </div>
+                <div>
+                    <FontAwesomeIcon icon={faCog} color="white" />
+                </div>
             </div>
         </>
     )
