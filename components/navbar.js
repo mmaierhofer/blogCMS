c87685a01498db361f99f25f7204355abbf48c3a
maierhoferma81177@th-nuernberg.de <maierhoferma81177@th-nuernberg.de>
2021-10-29 09:48:48 +0200

    add routing to pages

--- components/navbar.js
+++ components/navbar.js
@@ -1,21 +1,20 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faStream, faEuroSign, faUsers, faCog, faQuestion } from "@fortawesome/free-solid-svg-icons";
-import NavbarIcon from "./sideBar/sideBarIcon";
+import SideBarIcon from "./sideBar/sideBarIcon";
 
 export default function Navbar() {
     return (
         <>
             <div className={"w-16 fixed top-0 left-0 h-screen bg-primary flex-col justify-around items-center flex shadow-lg"}>
                 <div className="flex flex-col justify-around flex-1">
-                <NavbarIcon icon={faHome} tooltip="Home"/>
-                <NavbarIcon icon={faStream} tooltip="Products"/>
-                <NavbarIcon icon={faEuroSign} tooltip="Sales"/>
-                <NavbarIcon icon={faUsers} tooltip="Users"/>
-                <NavbarIcon icon={faCog} tooltip="Settings"/>
+                <SideBarIcon icon={faHome} tooltip="Home" link="/"/>
+                <SideBarIcon icon={faStream} tooltip="Products" link="/products"/>
+                <SideBarIcon icon={faEuroSign} tooltip="Sales" link="/sales"/>
+                <SideBarIcon icon={faUsers} tooltip="Users" link="/users"/>
+                <SideBarIcon icon={faCog} tooltip="Settings" link="/settings"/>
                 </div>
                 <div className="flex-1 justify-end flex flex-col">
-                <NavbarIcon icon={faQuestion} tooltip="Help"/>
+                <SideBarIcon icon={faQuestion} tooltip="Help" link="help"/>
                 </div>                
             </div>
         </>
--- components/sideBar/sideBarIcon.js
+++ components/sideBar/sideBarIcon.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from 'next/link'
 
-export default function NavbarIcon({icon, tooltip}) {
+
+export default function SideBarIcon({icon, tooltip, link}) {
     return (
         <>
-            <div className="sidebar-icon group">
+        <Link href={link}>
+        <div className="sidebar-icon group">
                 <FontAwesomeIcon icon={icon} />
                 <span className="sidebar-tooltip group-hover:scale-100">
                     {tooltip}
                 </span>
             </div>
+        </Link>
+            
         </>
     )
 }
