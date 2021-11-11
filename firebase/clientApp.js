2e6430a11e8d98402b734a500d3589ea01dc8404
maierhoferma81177@th-nuernberg.de <maierhoferma81177@th-nuernberg.de>
2021-11-11 11:22:55 +0100

    update firebase credentials

--- firebase/clientApp.js
+++ firebase/clientApp.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messegingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    apiKey: 'AIzaSyBSXyojHnLabZA_c-9GfY23jC-rnc0_5qw',
+    authDomain: 'next-webshop-e618d.firebaseapp.com',
+    projectId: 'next-webshop-e618d',
+    storageBucket: 'next-webshop-e618d.appspot.com',
+    messegingSenderId: '66976126262',
+    appId: '1:66976126262:web:791b7c6fee3844c4c34f97',
 }
 
 const app = initializeApp(firebaseConfig);
