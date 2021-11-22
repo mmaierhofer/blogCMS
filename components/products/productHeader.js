090f297e020deed926a8042f3c0000a609f35368
maierhoferma81177@th-nuernberg.de <maierhoferma81177@th-nuernberg.de>
2021-11-22 16:25:25 +0100

    fix header size

--- components/products/productHeader.js
+++ components/products/productHeader.js
@@ -5,7 +5,7 @@ export default function ProductHeader({activeTab, setActiveTab}) {
     return (
         <>
             <div className="w-full bg-white h-20 shadow-md flex">
-                <div className="flex justify-around items-center pl-5">
+                <div className="flex justify-around items-center pl-5 h-20">
                     <div className={"h-full flex flex-col items-center justify-center w-32 border-b-2 text-lg cursor-pointer " + (activeTab === "products" ? "border-lightPrimary text-lightPrimary" : "border-white text-black")} onClick={() => setActiveTab("products")}>
                         Products
                     </div>
