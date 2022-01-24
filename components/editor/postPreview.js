29ee249aae19a9a47f67c0b07cce869ee24a89b2
maierhoferma81177@th-nuernberg.de <mmaierhofer@outlook.de>
2022-01-24 21:12:22 +0100

    add alt to logo

--- components/editor/postPreview.js
+++ components/editor/postPreview.js
@@ -7,7 +7,9 @@ export default function PostPreview({ content }) {
     <>
       <div className="flex flex-row justify-center h-full items-start">
         <div className="bg-white w-80 h-5/6 border-4 p-2 rounded-xl border-black">
-          <div className="h-20 w-full"></div>
+          <div className="h-20 w-full flex flex-row justify-center">
+            <img alt="logo" />
+          </div>
           <div>
             <div
               className=" w-max-full w-fit text-justify"
