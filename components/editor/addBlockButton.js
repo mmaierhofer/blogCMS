9f04c3cc604a809cbe0ca171bd8ca633ca3911c9
maierhoferma81177@th-nuernberg.de <mmaierhofer@outlook.de>
2022-02-13 10:08:45 +0100

    add icon to component

--- components/editor/addBlockButton.js
+++ components/editor/addBlockButton.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 export default function AddBlockButton() {
   return (
     <>
       <div>
-        <div></div>
+        <div>
+          <FontAwesomeIcon icon={faPlus} />
+        </div>
         <div></div>
       </div>
     </>
