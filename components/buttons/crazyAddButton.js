97f937100ba501f5c5939a9643fb1065fb5b44d1
maierhoferma81177@th-nuernberg.de <mmaierhofer@outlook.de>
2022-02-15 09:11:55 +0100

    first design of animated add button

--- components/buttons/crazyAddButton.js
+++ components/buttons/crazyAddButton.js
@@ -1,5 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
 export default function CrazyAddButton() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <div className="absolute w-16 h-16 flex items-center justify-center text-2xl">
+          <FontAwesomeIcon color="#1c2039" icon={faPlus} />
+        </div>
+        <div className="flex flex-row">
+          <div className="w-8 h-8 rounded-tl-xl bg-white"></div>
+          <div className="w-8 h-8 rounded-tr-xl bg-white"></div>
+        </div>
+        <div className="flex flex-row">
+          <div className="w-8 h-8 rounded-bl-xl bg-white"></div>
+          <div className="w-8 h-8 rounded-br-xl bg-white"></div>
+        </div>
+      </div>
+    </>
+  );
 }
